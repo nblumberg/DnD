0ce487e7a96b1d5a125f9b22e8f6b98c092405c8
nblumberg <nathanielblumberg@gmail.com>
2013-12-20 03:27:01 -0500

    Upgrade karma to 0.10.4

--- karma.conf.js
+++ karma.conf.js
@@ -1,120 +1,200 @@
 // Karma configuration
-// Generated on Sat Jun 01 2013 17:43:52 GMT-0400 (EDT)
+// Generated on Wed Jun 05 2013 09:43:21 GMT-0400 (EDT)
+
+/* global module, process */
+
+module.exports = function(config) {
+    "use strict";
+
+    var options, libFiles, fixtures, coverageFiles, specFiles, i;
+    options = {
+        // base path, that will be used to resolve files and exclude
+        basePath: "",
+
+        frameworks: [ "jasmine", "commonjs" ],
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+        // list of files to exclude
+        exclude: [],
 
-// base path, that will be used to resolve files and exclude
-basePath = "";
+        // use dots reporter, as travis terminal does not support escaping sequences
+        // possible values: "dots", "progress"
+        // CLI --reporters progress
+        reporters: [
+            "progress", "junit", "coverage"
+        ],
 
-// All the Karma's urls get prefixed with the urlRoot. This is helpful when using proxies, as sometimes you might want to proxy a url that is already taken by Karma.
-urlRoot = "/";
+        junitReporter: {
+            // will be resolved to basePath (in the same way as files/exclude patterns)
+            outputFile: "test-results.xml"
+        },
 
-// list of files / patterns to load in the browser
-files = [
-	JASMINE,
-	JASMINE_ADAPTER,
+        // optionally, configure the reporter
+        coverageReporter: {
+            type: "html",
+            dir: "coverage/"
+        },
 
-	// application files to unit test
-	"src/main/webapp/static/js/lib/jquery.min.js",
-    "src/test/webapp/jasmine/lib/jasmine-jquery.js",
-	"src/main/webapp/static/js/js.js",
-	"src/main/webapp/static/js/serializable.js",
-    "src/main/webapp/static/js/event.js",
-    "src/main/webapp/static/js/effect.js",
-	"src/main/webapp/static/js/actions.js",
-	"src/main/webapp/static/js/editor.js",
-	"src/main/webapp/static/js/creature.js",
-	"src/main/webapp/static/js/data.js",
-	"src/main/webapp/static/js/monsters.js",
-    "src/main/webapp/static/js/party.js",
-    "src/main/webapp/static/js/history.js",
+        // web server port
+        // CLI --port 9876
+        port: 9876,
 
-    // fixtures
-    {
-        pattern: "src/main/webapp/static/html/partials/**/*.html",
-        watched: true,
-        included: false,
-        served: true
-    },
-    {
-        pattern: "src/test/webapp/jasmine/spec/javascripts/fixtures/**/*.html",
-        watched: true,
-        included: false,
-        served: true
-    },
+        // enable / disable colors in the output (reporters and logs)
+        // CLI --colors --no-colors
+        colors: true,
 
-	// test specs to run
-    "src/test/webapp/jasmine/spec/testUtilities.js",
-    "src/test/webapp/jasmine/spec/creatureTestUtilities.js",
-	"src/test/webapp/jasmine/spec/jsTest.js",
-    "src/test/webapp/jasmine/spec/serializableTest.js",
-    "src/test/webapp/jasmine/spec/eventTest.js",
-    "src/test/webapp/jasmine/spec/effectTest.js",
-    "src/test/webapp/jasmine/spec/actionsTest.js",
-	"src/test/webapp/jasmine/spec/editorTest.js",
-    "src/test/webapp/jasmine/spec/dataTest.js",
-	"src/test/webapp/jasmine/spec/monstersTest.js",
-    "src/test/webapp/jasmine/spec/partyTest.js",
-    "src/test/webapp/jasmine/spec/historyTest.js"
-];
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        // CLI --log-level debug
+        logLevel: config.LOG_INFO,
 
+        // enable / disable watching file and executing tests whenever any file changes
+        // CLI --auto-watch --no-auto-watch
+        autoWatch: true,
 
-// list of files to exclude
-exclude = [
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        // CLI --browsers Chrome,Firefox,Safari
+        browsers: [
+            process.env.TRAVIS ? "Firefox" : "Chrome"
+        ],
 
-];
+        // If browser does not capture in given timeout [ms], kill it
+        // CLI --capture-timeout 5000
+        captureTimeout: 20000,
 
+        // Auto run tests on start (when browsers are captured) and exit
+        // CLI --single-run --no-single-run
+        singleRun: false,
 
-// Hostname to be used when capturing browsers.
-hostname = "localhost";
+        // report which specs are slower than 500ms
+        // CLI --report-slower-than 500
+        reportSlowerThan: 500,
 
-// The port where the webserver will be listening.
-port = 9876;
+        plugins: [
+            "karma-jasmine",
+            "karma-chrome-launcher",
+            "karma-firefox-launcher",
+            "karma-junit-reporter",
+            "karma-commonjs",
+            "karma-coverage"
+        ]
+    };
 
-// The port where the server will be listening. This is only used when you are using karma run.
-runnerPort = 9100;
+    // list of files / patterns to load in the browser
+    files: [
+        "src/main/webapp/static/js/js.js",
+        "src/main/webapp/static/js/serializable.js",
+        "src/main/webapp/static/js/event.js",
+        "src/main/webapp/static/js/effect.js",
+        "src/main/webapp/static/js/actions.js",
+        "src/main/webapp/static/js/editor.js",
+        "src/main/webapp/static/js/creature.js",
+        "src/main/webapp/static/js/data.js",
+        "src/main/webapp/static/js/monsters.js",
+        "src/main/webapp/static/js/party.js",
+        "src/main/webapp/static/js/history.js",
 
-// A map of path-proxy pairs.
-//proxies =  {
-//    "/static": "http://gstatic.com",
-//    "/web": "http://localhost:9000"
-//};
+        // fixtures
+        {
+            pattern: "src/main/webapp/static/html/partials/**/*.html",
+            watched: true,
+            included: false,
+            served: true
+        },
+        {
+            pattern: "src/test/webapp/jasmine/spec/javascripts/fixtures/**/*.html",
+            watched: true,
+            included: false,
+            served: true
+        },
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+        // test specs to run
+        "src/test/webapp/jasmine/spec/testUtilities.js",
+        "src/test/webapp/jasmine/spec/creatureTestUtilities.js",
+        "src/test/webapp/jasmine/spec/jsTest.js",
+        "src/test/webapp/jasmine/spec/serializableTest.js",
+        "src/test/webapp/jasmine/spec/eventTest.js",
+        "src/test/webapp/jasmine/spec/effectTest.js",
+        "src/test/webapp/jasmine/spec/actionsTest.js",
+        "src/test/webapp/jasmine/spec/editorTest.js",
+        "src/test/webapp/jasmine/spec/dataTest.js",
+        "src/test/webapp/jasmine/spec/monstersTest.js",
+        "src/test/webapp/jasmine/spec/partyTest.js",
+        "src/test/webapp/jasmine/spec/historyTest.js"
+    ],
 
-// A list of log appenders to be used. See the documentation for log4js for more information.
-loggers = [ { type: "console" } ];
+    // application/lib dependencies
+    libFiles = [
+        "src/main/webapp/static/js/lib/jquery.min.js",
+        "src/test/webapp/jasmine/lib/jasmine-jquery.js"
+    ];
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    fixtures = [
+        {
+            pattern: "src/main/webapp/static/html/partials/**/*.html",
+            watched: true,
+            included: false,
+            served: true
+        },
+        {
+            pattern: "src/test/webapp/jasmine/spec/javascripts/fixtures/**/*.html",
+            watched: true,
+            included: false,
+            served: true
+        }
+    ];
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = [ "Chrome" ]; // , "Firefox", "Safari", "PhantomJS" ];
+    coverageFiles = [
+        "src/main/webapp/static/js/js.js",
+        "src/main/webapp/static/js/serializable.js",
+        "src/main/webapp/static/js/event.js",
+        "src/main/webapp/static/js/effect.js",
+        "src/main/webapp/static/js/actions.js",
+        "src/main/webapp/static/js/editor.js",
+        "src/main/webapp/static/js/creature.js",
+        "src/main/webapp/static/js/data.js",
+        "src/main/webapp/static/js/monsters.js",
+        "src/main/webapp/static/js/party.js",
+        "src/main/webapp/static/js/history.js"
+    ];
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    // test specs to run
+    specFiles = [
+        "src/test/webapp/jasmine/spec/testUtilities.js",
+        "src/test/webapp/jasmine/spec/creatureTestUtilities.js",
+        "src/test/webapp/jasmine/spec/jsTest.js",
+        "src/test/webapp/jasmine/spec/serializableTest.js",
+        "src/test/webapp/jasmine/spec/eventTest.js",
+        "src/test/webapp/jasmine/spec/effectTest.js",
+        "src/test/webapp/jasmine/spec/actionsTest.js",
+        "src/test/webapp/jasmine/spec/editorTest.js",
+        "src/test/webapp/jasmine/spec/dataTest.js",
+        "src/test/webapp/jasmine/spec/monstersTest.js",
+        "src/test/webapp/jasmine/spec/partyTest.js",
+        "src/test/webapp/jasmine/spec/historyTest.js"
+    ];
 
-// Karma will report all the tests that are slower than given time limit (in ms). This is disabled by default.
-//reportSlowerThan = 0;
+    // list of files / patterns to load in the browser
+    options.files = libFiles.concat(fixtures).concat(coverageFiles).concat(specFiles);
 
-//A map of preprocessors to use. See config/preprocessors for more.
-//preprocessors = { "**/*.coffee": "coffee" };
+    /**
+     * Coverage Report instructions from:
+     * https://github.com/karma-runner/karma-coverage
+     */
+    // source files, that you wanna generate coverage for
+    // do not include tests or libraries
+    // (these files will be instrumented by Istanbul)
+    options.preprocessors = {};
+    for (i = 0; i < coverageFiles.length; i++) {
+        options.preprocessors[ coverageFiles[ i ] ] = "coverage";
+    }
 
-//A list of reporters to use.
-//possible values: "dots", "progress", "junit", "growl", "coverage"
-reporters = [ "progress" ];
-
-// Continuous Integration mode.
-// If true, it captures browsers, runs tests and exits with 0 exit code (if all tests passed) or 1 exit code (if any test failed).
-singleRun = false;
+    config.set(options);
+};
