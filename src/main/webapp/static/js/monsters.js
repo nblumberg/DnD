6cedfed4e30cf21914cb36a8dd50439703793488
Nathaniel Blumberg <nblumberg@constantcontact.com>
2014-11-29 15:22:54 -0500

    Add Seed of Winter to monsters.js

--- src/main/webapp/static/js/monsters.js
+++ src/main/webapp/static/js/monsters.js
@@ -1311,6 +1311,33 @@
                         ], keywords: [ "acid" ] }
                     ]
                 },
+                "Seed of Winter": {
+                    name: "Seed of Winter", level: 18, image: "../images/portraits/seed_of_winter.png",
+                    hp: { total: 1 },
+                    defenses: { ac: 10, fort: 10, ref: 10, will: 10 },
+                    init: 8, speed: { walk: 0 },
+                    abilities: { STR: 10, CON: 10, DEX: 10, INT: 10, WIS: 10, CHA: 10 },
+                    skills: { acrobatics: 0, arcana: 0, athletics: 0, bluff: 0, diplomacy: 0, dungeoneering: 0, endurance: 0, heal: 0, history: 0, insight: 0, intimidate: 0, nature: 0, perception: 0, religion: 0, stealth: 0, streetwise: 0, thievery: 0 },
+                    attacks: [
+                        {
+                            name: "Trap",
+                            usage: { frequency: "At-Will" }, range: "melee",
+                            toHit: 24, defense: "Will", damage: "0",
+                            effects: [
+                                { name: "dominated", saveEnds: true, afterEffects: [
+                                    { name: "immobilized", saveEnds: true }
+                                ] }
+                            ], keywords: [ "melee", "ranged", "basic" ]
+                        },
+                        {
+                            name: "Trap (after effect)",
+                            usage: { frequency: "At-Will" }, range: "melee",
+                            toHit: "automatic", defense: "Will", damage: { amount: "2d10+5", type: "cold" },
+//                            effects: [ { name: "immobilized", saveEnds: true } ],
+                            keywords: [ "ranged" ]
+                        }
+                    ]
+                },
                 "Shadar-kai Warrior": {
                     name: "Shadar-kai Warrior", level: 8, image: "../images/portraits/shadar_kai_warrior.png",
                     hp: { total: 86 },
